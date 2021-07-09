ff1e4f73a392660a0f9f9a728c199caadfe9aefe
eric.tang <eric.tang@starfivetech.com>
2021-07-09 10:13:36 +0800

    target/riscv: rvb: add generalized shuffle
    Signed-off-by: eric.tang <eric.tang@starfivetech.com>

--- target/riscv/bitmanip_helper.c
+++ target/riscv/bitmanip_helper.c
@@ -188,3 +188,135 @@ target_ulong HELPER(fsrw)(target_ulong rs1, target_ulong rs2, target_ulong rs3)
 {
     return do_fslw(rs1, -rs2, rs3);
 }
+static target_ulong do_shfl(target_ulong rs1,
+                            target_ulong rs2,
+                            int bits)
+{
+    target_ulong x = rs1;
+    int shamt = rs2 & ((bits-1) >> 1);
+    if (shamt & 16)
+        x = (x & 0xFFFF00000000FFFFLL) |
+            ((x & 0x0000FFFF00000000LL) >> 16) |
+            ((x & 0x00000000FFFF0000LL) << 16);
+    if (shamt &  8)
+        x = (x & 0xFF0000FFFF0000FFLL) |
+            ((x & 0x00FF000000FF0000LL) >>  8) |
+            ((x & 0x0000FF000000FF00LL) <<  8);
+    if (shamt &  4)
+        x = (x & 0xF00FF00FF00FF00FLL) |
+            ((x & 0x0F000F000F000F00LL) >>  4) |
+            ((x & 0x00F000F000F000F0LL) <<  4);
+    if (shamt &  2)
+        x = (x & 0xC3C3C3C3C3C3C3C3LL) |
+            ((x & 0x3030303030303030LL) >>  2) |
+            ((x & 0x0C0C0C0C0C0C0C0CLL) <<  2);
+    if (shamt &  1)
+        x = (x & 0x9999999999999999LL) |
+            ((x & 0x4444444444444444LL) >>  1) |
+            ((x & 0x2222222222222222LL) <<  1);
+
+    return x;
+}
+
+static target_ulong do_unshfl(target_ulong rs1,
+                              target_ulong rs2,
+                              int bits)
+{
+    target_ulong x = rs1;
+
+    int shamt = rs2 & ((bits-1) >> 1);
+    if (shamt &  1)
+        x = (x & 0x9999999999999999LL) |
+            ((x & 0x4444444444444444LL) >>  1) |
+            ((x & 0x2222222222222222LL) <<  1);
+    if (shamt &  2)
+        x = (x & 0xC3C3C3C3C3C3C3C3LL) |
+            ((x & 0x3030303030303030LL) >>  2) |
+            ((x & 0x0C0C0C0C0C0C0C0CLL) <<  2);
+    if (shamt &  4)
+        x = (x & 0xF00FF00FF00FF00FLL) |
+            ((x & 0x0F000F000F000F00LL) >>  4) |
+            ((x & 0x00F000F000F000F0LL) <<  4);
+    if (shamt &  8)
+        x = (x & 0xFF0000FFFF0000FFLL) |
+            ((x & 0x00FF000000FF0000LL) >>  8) |
+            ((x & 0x0000FF000000FF00LL) <<  8);
+    if (shamt & 16)
+        x = (x & 0xFFFF00000000FFFFLL) |
+            ((x & 0x0000FFFF00000000LL) >> 16) |
+            ((x & 0x00000000FFFF0000LL) << 16);
+
+    return x;
+}
+
+static target_ulong do_shflw(target_ulong rs1,
+                             target_ulong rs2,
+                             int bits)
+{
+    target_ulong x = rs1;
+    int shamt = rs2 & 15;
+    if (shamt & 8)
+        x = (x & 0xFF0000FFFF0000FFLL) |
+            ((x & 0x00FF000000FF0000LL) >>  8) |
+            ((x & 0x0000FF000000FF00LL) <<  8);
+    if (shamt & 4)
+        x = (x & 0xF00FF00FF00FF00FLL) |
+            ((x & 0x0F000F000F000F00LL) >>  4) |
+            ((x & 0x00F000F000F000F0LL) <<  4);
+    if (shamt & 2)
+        x = (x & 0xC3C3C3C3C3C3C3C3LL) |
+            ((x & 0x3030303030303030LL) >>  2) |
+            ((x & 0x0C0C0C0C0C0C0C0CLL) <<  2);
+    if (shamt & 1)
+        x = (x & 0x9999999999999999LL) |
+            ((x & 0x4444444444444444LL) >>  1) |
+            ((x & 0x2222222222222222LL) <<  1);
+
+    return x;
+}
+
+static target_ulong do_unshflw(target_ulong rs1,
+                               target_ulong rs2,
+                               int bits)
+{
+    target_ulong x = rs1;
+    int shamt = rs2 & 15;
+    if (shamt & 1)
+        x = (x & 0x9999999999999999LL) |
+            ((x & 0x4444444444444444LL) >>  1) |
+            ((x & 0x2222222222222222LL) <<  1);
+    if (shamt & 2)
+        x = (x & 0xC3C3C3C3C3C3C3C3LL) |
+            ((x & 0x3030303030303030LL) >>  2) |
+            ((x & 0x0C0C0C0C0C0C0C0CLL) <<  2);
+    if (shamt & 4)
+        x = (x & 0xF00FF00FF00FF00FLL) |
+            ((x & 0x0F000F000F000F00LL) >>  4) |
+            ((x & 0x00F000F000F000F0LL) <<  4);
+    if (shamt & 8)
+        x = (x & 0xFF0000FFFF0000FFLL) |
+            ((x & 0x00FF000000FF0000LL) >>  8) |
+            ((x & 0x0000FF000000FF00LL) <<  8);
+
+    return x;
+}
+
+target_ulong HELPER(shfl)(target_ulong rs1, target_ulong rs2)
+{
+    return do_shfl(rs1, rs2, TARGET_LONG_BITS);
+}
+
+target_ulong HELPER(unshfl)(target_ulong rs1, target_ulong rs2)
+{
+    return do_unshfl(rs1, rs2, TARGET_LONG_BITS);
+}
+
+target_ulong HELPER(shflw)(target_ulong rs1, target_ulong rs2)
+{
+    return do_shflw(rs1, rs2, TARGET_LONG_BITS);
+}
+
+target_ulong HELPER(unshflw)(target_ulong rs1, target_ulong rs2)
+{
+    return do_unshflw(rs1, rs2, TARGET_LONG_BITS);
+}
